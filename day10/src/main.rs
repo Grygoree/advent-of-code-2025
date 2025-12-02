3ef97c070387d8723fb783a7d9e5581f6bede3e5
Gregory Leung <not.gregs.other.email@gmail.com>
2025-12-02 07:49:52 -0600

    Ran scaffolding script

---
+++ day10/src/main.rs
@@ -0,0 +1,17 @@
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+pub fn part1(input: &str) -> i64 {
+    // TODO
+    0
+}
+
+pub fn part2(input: &str) -> i64 {
+    // TODO
+    0
+}
