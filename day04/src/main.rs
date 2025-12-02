b2c218d1825bd3265b46294040133140f62d4cee
Gregory Leung <not.gregs.other.email@gmail.com>
2025-12-02 08:01:09 -0600

    Fix script

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,6 +1,6 @@
 
 fn main() {
-    let input = include_str!("input.txt");
+    let input = include_str!("../input.txt");
 
     println!("Part 1: {}", part1(&input));
     println!("Part 2: {}", part2(&input));
--- scaffold.sh
+++ scaffold.sh
@@ -4,15 +4,15 @@ set -e
 DAYS=31
 
 for i in $(seq -w 1 $DAYS); do
-	name="day$i"
-	echo "Creating $name..."
-	cargo new --bin --vcs none "$name"
+  name="day$i"
+  echo "Creating $name..."
+  cargo new --bin --vcs none "$name"
 
-	touch "$name/input.txt"
-	cat > "$name/src/main.rs" << 'MAIN'
+  touch "$name/input.txt"
+  cat >"$name/src/main.rs" <<'MAIN'
 
 fn main() {
-    let input = include_str!("input.txt");
+    let input = include_str!("../input.txt");
 
     println!("Part 1: {}", part1(&input));
     println!("Part 2: {}", part2(&input));
